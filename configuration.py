d9aa4925938911ee5fbd12730df54f7b7740a31b
E-TorresC <109243545+E-TorresC@users.noreply.github.com>
2024-08-10 15:47:14 -0500

    Update configuration.py URL

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-63fab913-ed17-4c4e-90fe-e6a698222519.containerhub.tripleten-services.com/"
+URL_SERVICE = "https://cnt-6fe2bec7-3d9c-4da7-af50-08d65fa2fed6.containerhub.tripleten-services.com/"
 CREATE_USER_PATH = "/api/v1/users/"
 KITS_PATH = "/api/v1/kits"
